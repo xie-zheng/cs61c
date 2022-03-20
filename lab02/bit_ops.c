c81e3459ca1580cb8a8b0016e8447844bb93e312
xiezheng <xiezheng@live.com>
2022-03-20 16:13:12 +0800

    [lab02] finish #learn some docker usage

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -4,24 +4,37 @@
 // Return the nth bit of x.
 // Assume 0 <= n <= 31
 unsigned get_bit(unsigned x,
-                 unsigned n) {
-    // YOUR CODE HERE
-    // Returning -1 is a placeholder (it makes
-    // no sense, because get_bit only returns 
-    // 0 or 1)
-    return -1;
-}
-// Set the nth bit of the value of x to v.
-// Assume 0 <= n <= 31, and v is 0 or 1
-void set_bit(unsigned * x,
-             unsigned n,
-             unsigned v) {
-    // YOUR CODE HERE
-}
-// Flip the nth bit of the value of x.
-// Assume 0 <= n <= 31
-void flip_bit(unsigned * x,
-              unsigned n) {
-    // YOUR CODE HERE
+                 unsigned n)
+{
+    return (x & (1 << n)) >> n;
 }
 
+// Set the nth bit of the value of x to v.
+// Assume 0 <= n <= 31, and v is 0 or 1
+void set_bit(unsigned *x,
+             unsigned n,
+             unsigned v)
+{
+    unsigned bit = get_bit(*x, n);
+    if (bit == v)
+    {
+        return;
+    }
+    flip_bit(x, n);
+}
+
+// Flip the nth bit of the value of x.
+// Assume 0 <= n <= 31
+void flip_bit(unsigned *x,
+              unsigned n)
+{
+    unsigned bit = get_bit(*x, n);
+    if (bit == 0)
+    {
+        *x = *x | (1 << n);
+    }
+    else
+    {
+        *x = *x ^ (1 << n);
+    }
+}
--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -4,7 +4,8 @@
 #include <string.h>
 #include "lfsr.h"
 
-void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+void lfsr_calculate(uint16_t *reg)
+{
+    uint16_t next_bit = (*reg ^ (*reg >> 2) ^ (*reg >> 3) ^ (*reg >> 5)) & 1;
+    *reg = (*reg >> 1) | (next_bit << 15);
 }
-
