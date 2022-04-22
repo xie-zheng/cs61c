2053eb941731ce0f54567a2bd5cfed7729368cbe
xiezheng <xiezheng@live.com>
2022-04-22 21:31:22 +0800

    [lab03] ex1-2

--- lab03/ex2.c
+++ lab03/ex2.c
@@ -1,16 +1,19 @@
 int source[] = {3, 1, 4, 1, 5, 9, 0};
 int dest[10];
 
-int fun(int x) {
-	return -x * (x + 1);
+int fun(int x)
+{
+    return -x * (x + 1); // result in a0
 }
 
-int main() {
-    int k;
-    int sum = 0;
-    for (k = 0; source[k] != 0; k++) {
-        dest[k] = fun(source[k]);
-        sum += dest[k];
+int main()
+{
+    int k;                           // s0
+    int sum = 0;                     // t0
+    for (k = 0; source[k] != 0; k++) // s3 = k * 4, shift
+    {
+        dest[k] = fun(source[k]); // source[k] in t2
+        sum += dest[k];           // dest[k] in t2 too
     }
     return sum;
 }
