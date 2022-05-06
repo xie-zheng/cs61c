a76ca7eeddec74abfcb804b2eb9ccc60d4993c7f
xiezheng <xiezheng@live.com>
2022-05-06 15:42:28 +0800

    [lab04] ex1

--- lab04/megalistmanips.s
+++ lab04/megalistmanips.s
@@ -65,21 +65,27 @@ map:
     # also keep in mind that we should not make ANY assumption on which registers
     # are modified by the callees, even when we know the content inside the functions 
     # we call. this is to enforce the abstraction barrier of calling convention.
-mapLoop:
-    add t1, s0, x0      # load the address of the array of current node into t1
-    lw t2, 4(s0)        # load the size of the node's array into t2
+    
 
-    add t1, t1, t0      # offset the array address by the count
+
+mapLoop:
+    lw t1, 0(s0)        # load the address of the array of current node into t1
+    lw t2, 4(s0)        # load the size of the node's array into t2
+    slli t3, t0, 2 		# offset the array address by the count
+    add t1, t1, t3
     lw a0, 0(t1)        # load the value at that address into a0
 
     jalr s1             # call the function on that value.
 
+	# t1 had been overwrite by mystery
     sw a0, 0(t1)        # store the returned value back into the array
     addi t0, t0, 1      # increment the count
-    bne t0, t2, mapLoop # repeat if we haven't reached the array size yet
-
-    la a0, 8(s0)        # load the address of the next node into a0
-    lw a1, 0(s1)        # put the address of the function back into a1 to prepare for the recursion
+    
+    #addi t1, t1, 4 
+    blt t0, t2, mapLoop # repeat if we haven't reached the array size yet
+    
+    lw a0, 8(s0)        # load the address of the next node into a0
+    mv a1, s1           # put the address of the function back into a1 to prepare for the recursion
 
     jal  map            # recurse
 done:
@@ -90,8 +96,8 @@ done:
     jr ra
 
 mystery:
-    mul t1, a0, a0
-    add a0, t1, a0
+    mul t3, a0, a0
+    add a0, t3, a0
     jr ra
 
 create_default_list:
